3c0aeaef62bebad131fcfb37856da903e4db300b
ValliereMagic <ValliereMagic@users.noreply.github.com>
2021-10-09 17:16:05 -0400

    Update interface; setup encryption with symmetric keyfiles; create functionality of generating asymmetric keypairs; Update ReadMe.

--- mala_cryptor/src/key_file.rs
+++ mala_cryptor/src/key_file.rs
@@ -1,11 +1,34 @@
-use oqs::kem::{Algorithm, Kem};
-use sodiumoxide::crypto::kx;
+use kem::Kem;
+use oqs::kem;
+use oqs::sig;
+use sig::Sig;
+use sodiumoxide::crypto::{
+	kx, secretstream::gen_key, secretstream::Key, secretstream::KEYBYTES, sign,
+};
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Result;
 
+pub fn gen_symmetric_keyfile(key_file_path: &str) -> Result<()> {
+	sodiumoxide::init().expect("Unable to initialize libsodium.");
+	let mut key_file = File::create(&key_file_path)?;
+	let key = gen_key();
+	key_file.write_all(&key.0)?;
+	Ok(())
+}
+
+pub fn get_symmetric_keyfile(key_file_path: &str) -> Result<Key> {
+	let mut key_file = File::open(&key_file_path)?;
+	let mut key = Key([0u8; KEYBYTES]);
+	key_file.read_exact(&mut key.0)?;
+	Ok(key)
+}
+
 // File format:
 // [qkey][classical_key]
+// where:
+// qkey: [q_sig_key][q_kem_key]
+// classical_key: [c_sig_key][c_kem_key]
 pub fn gen_hybrid_keypair(pkey_path: &str, skey_path: &str) -> Result<()> {
 	let (mut pkey_f, mut skey_f) = (File::create(pkey_path)?, File::create(skey_path)?);
 	quantum(&mut pkey_f, &mut skey_f)?;
@@ -25,18 +48,27 @@ pub fn gen_classical_keypair(pkey_path: &str, skey_path: &str) -> Result<()> {
 
 fn quantum(pkey_f: &mut File, skey_f: &mut File) -> Result<()> {
 	oqs::init();
-	let kem =
-		Kem::new(Algorithm::ClassicMcEliece6688128f).expect("Unable to acquire quantum KEM algo.");
+	let kem = Kem::new(kem::Algorithm::ClassicMcEliece6688128f)
+		.expect("Unable to acquire quantum KEM algo.");
+	let sig = Sig::new(sig::Algorithm::Dilithium5).expect("Unable to acquire quantum SIG algo.");
+	let (sig_pkey, sig_skey) = sig
+		.keypair()
+		.expect("Unable to generate quantum signature keypair.");
 	let (pkey, skey) = kem.keypair().expect("Unable to generate quantum keypair.");
+	pkey_f.write_all(&sig_pkey.into_vec())?;
 	pkey_f.write_all(&pkey.into_vec())?;
+	skey_f.write_all(&sig_skey.into_vec())?;
 	skey_f.write_all(&skey.into_vec())?;
 	Ok(())
 }
 
 fn classical(pkey_f: &mut File, skey_f: &mut File) -> Result<()> {
 	sodiumoxide::init().expect("Unable to initialize libsodium.");
+	let (sig_pkey, sig_skey) = sign::gen_keypair();
 	let (pkey, skey) = kx::gen_keypair();
+	pkey_f.write_all(&sig_pkey.0)?;
 	pkey_f.write_all(&pkey.0)?;
+	skey_f.write_all(&sig_skey.0)?;
 	skey_f.write_all(&skey.0)?;
 	Ok(())
 }
--- mala_cryptor/src/main.rs
+++ mala_cryptor/src/main.rs
@@ -3,71 +3,191 @@ mod key_derivation;
 mod key_file;
 
 use clap::{App, AppSettings, SubCommand};
-use file_symmetric_encryption::{decrypt_file_with_password, encrypt_file_with_password};
+use file_symmetric_encryption::*;
+use key_file::*;
 use rpassword::prompt_password_stdout;
 use std::io::{Error, ErrorKind, Result};
 
 fn main() -> Result<()> {
 	let matches = App::new("mala_cryptor")
-		.version("0.1.0")
+		.version("0.2.0")
 		.author("ValliereMagic")
 		.about("A command line file cryptography tool")
 		.setting(AppSettings::ArgRequiredElseHelp)
-		.subcommand(SubCommand::with_name("sym").setting(AppSettings::ArgRequiredElseHelp)
+		// Symmetric File encryption (With password or file)
+		.subcommand(SubCommand::with_name("sym")
+				.setting(AppSettings::ArgRequiredElseHelp)
 				.about("symmetric file encryption with a key_file or password")
-				.args_from_usage(
-					"-e, --encrypt=[FILENAME] 'specify a file to encrypt'
-					-d, --decrypt=[FILENAME] 'specify a file to decrypt'
-					-o, --output=[FILENAME] 'the target file to write the resultant file to'",
-				))
-		.subcommand(SubCommand::with_name("pub").setting(AppSettings::ArgRequiredElseHelp)
+				// Encryption of files
+				.subcommand(SubCommand::with_name("enc")
+						.setting(AppSettings::ArgRequiredElseHelp)
+						.about("encrypt a file")
+						.args_from_usage(
+							"-k, --key_file=[FILENAME] 'specify a key file to use (if not specified, you will be prompted for a password)'
+							-i, --in_file=[FILENAME] 'specify a file to encrypt'
+							-o, --out_file=[FILENAME] 'specify an output filename'"
+						)
+				)
+				// Decryption of files
+				.subcommand(SubCommand::with_name("dec")
+						.setting(AppSettings::ArgRequiredElseHelp)
+						.about("decrypt a file")
+						.args_from_usage(
+							"-k, --key_file=[FILENAME] 'specify a key file to use (if not specified, you will be prompted for a password)'
+							-i, --in_file=[FILENAME] 'specify a file to decrypt'
+							-o, --out_file=[FILENAME] 'specify an output filename'"
+						)
+				)
+				// Generation of a keyfile
+				.subcommand(SubCommand::with_name("gen")
+						.setting(AppSettings::ArgRequiredElseHelp)
+						.about("Generate a symmetric keyfile")
+						.arg_from_usage("-o, --out_file=[FILENAME] 'specify an output filename'")
+				)
+		)
+		// Public-Private keypair encryption (With keyfiles)
+		.subcommand(SubCommand::with_name("pub")
+				.setting(AppSettings::ArgRequiredElseHelp)
 				.about("Public-Private key file encryption")
-				.args_from_usage(
-					"-m, --mode=[MODE] 'specify the mode to use. Options: [quantum, classic, hybrid]'
-					-e, --encrypt=[FILENAME] 'specify a file to encrypt'
-					-d, --decrypt=[FILENAME] 'specify a file to decrypt'
-					-p, --private=[FILENAME] 'specify a public key file'
-					-s, --secret=[FILENAME] 'specify a secret key file'
-					-r, --recipient=[FILENAME] 'specify the recipient's public key'"
-				))
+				// Keypair generation
+				.subcommand(SubCommand::with_name("gen")
+						.setting(AppSettings::ArgRequiredElseHelp)
+						.about("Generate a public-private keypair")
+						.args_from_usage(
+							"-m, --mode=['q', 'c', 'h'] 'specify type of keypair to generate: q: quantum, c: classical, h: hybrid (both, in cascade)'
+							-s, --secret_key=[Output FILENAME] 'specify the output secret key filename'
+							-p, --public_key=[Output FILENAME] 'specify the output public key filename'"
+						)
+				)
+				// Encryption and Signing
+				.subcommand(SubCommand::with_name("enc")
+						.setting(AppSettings::ArgRequiredElseHelp)
+						.about("Encrypt a file using a public key [and sign]")
+						.args_from_usage(
+							"-m, --mode=['q', 'c', 'h'] 'specify type of keypair to generate: q: quantum, c: classical, h: hybrid (both, in cascade)'
+							-d, --destination=[Input FILENAME] 'specify the public key of the recipient'
+							-s, --secret_key=[Input FILENAME] 'specify the secret key to sign with'
+							-p, --public_key=[Input FILENAME] 'specify our public key for key exchange'
+							-i, --in_file=[FILENAME] 'specify a file to encrypt'
+							-o, --out_file=[FILENAME] 'specify an output filename'"
+						)
+				)
+				// Decryption and Signing
+				.subcommand(SubCommand::with_name("dec")
+						.setting(AppSettings::ArgRequiredElseHelp)
+						.about("Decrypt a file using a public key [and verify signature]")
+						.args_from_usage(
+							"-m, --mode=['q', 'c', 'h'] 'specify type of keypair to generate: q: quantum, c: classical, h: hybrid (both, in cascade)'
+							-f, --from=[Input FILENAME] 'specify the public key of the sender'
+							-s, --secret_key=[Input FILENAME] 'specify the secret key to sign with'
+							-p, --public_key=[Input FILENAME] 'specify our public key for key exchange'
+							-i, --in_file=[FILENAME] 'specify a file to encrypt'
+							-o, --out_file=[FILENAME] 'specify an output filename'"
+						)
+				)
+		)
 		.get_matches();
-	// Figure out what the user wants to do...
-	if let Some(matches) = matches.subcommand_matches("sym") {
-		enum Mode {
-			Encrypt,
-			Decrypt,
+	// The user is doing symmetric encryption
+	if let Some(sym) = matches.subcommand_matches("sym") {
+		// Get the options from clap (need to do for both enc and dec)
+		fn get_sym_options<'a>(
+			options: &'a clap::ArgMatches,
+		) -> Result<(Option<&'a str>, &'a str, &'a str)> {
+			// Get the options from the user
+			let key_file = options.value_of("key_file");
+			let in_file = match options.value_of("in_file") {
+				Some(f) => f,
+				None => {
+					return Err(Error::new(
+						ErrorKind::Other,
+						"Input file must be specified.",
+					));
+				}
+			};
+			let out_file = match options.value_of("out_file") {
+				Some(f) => f,
+				None => {
+					return Err(Error::new(
+						ErrorKind::Other,
+						"Output file must be specified.",
+					));
+				}
+			};
+			Ok((key_file, in_file, out_file))
 		}
-		let (mode, in_file) = if let Some(encrypt) = matches.value_of("encrypt") {
-			(Mode::Encrypt, encrypt)
-		} else if let Some(decrypt) = matches.value_of("decrypt") {
-			(Mode::Decrypt, decrypt)
-		} else {
-			return Err(Error::new(
-				ErrorKind::Other,
-				"Either encrypt or decrypt must be specified.",
-			));
-		};
-		let out_file: &str = match matches.value_of("output") {
-			Some(o) => o,
-			None => {
-				return Err(Error::new(
-					ErrorKind::Other,
-					"Output file must be specified.",
-				));
-			}
-		};
-		// Get the password from the user
-		let pass = prompt_password_stdout("Enter the file password: ").unwrap();
-		match mode {
-			Mode::Encrypt => {
+		// User is encrypting a file
+		if let Some(enc) = sym.subcommand_matches("enc") {
+			let (key_file, in_file, out_file) = get_sym_options(enc)?;
+			if let Some(key_file_path) = key_file {
+				encrypt_file_with_key(in_file, out_file, get_symmetric_keyfile(key_file_path)?)?;
+			} else {
+				// Get the password from the user
+				let pass = prompt_password_stdout("Enter the file password: ").unwrap();
 				encrypt_file_with_password(in_file, out_file, &pass)?;
 			}
-			Mode::Decrypt => {
+		// User is decrypting a file
+		} else if let Some(dec) = sym.subcommand_matches("dec") {
+			let (key_file, in_file, out_file) = get_sym_options(dec)?;
+			if let Some(key_file_path) = key_file {
+				decrypt_file_with_key(in_file, out_file, get_symmetric_keyfile(key_file_path)?)?;
+			} else {
+				// Get the password from the user
+				let pass = prompt_password_stdout("Enter the file password: ").unwrap();
 				decrypt_file_with_password(in_file, out_file, &pass)?;
 			}
+		// User is generating a keyfile
+		} else if let Some(gen) = sym.subcommand_matches("gen") {
+			let out_file = match gen.value_of("out_file") {
+				Some(f) => f,
+				None => {
+					return Err(Error::new(
+						ErrorKind::Other,
+						"Output file must be specified.",
+					));
+				}
+			};
+			gen_symmetric_keyfile(&out_file)?;
 		}
-	} else if let Some(_) = matches.subcommand_matches("pub") {
-		println!("Not yet implemented!");
+	// The User is doing public key encryption
+	} else if let Some(public) = matches.subcommand_matches("pub") {
+		if let Some(gen) = public.subcommand_matches("gen") {
+			let secret_key = match gen.value_of("secret_key") {
+				Some(s) => s,
+				None => {
+					return Err(Error::new(
+						ErrorKind::Other,
+						"A Secret Key file must be specified.",
+					));
+				}
+			};
+			let public_key = match gen.value_of("public_key") {
+				Some(p) => p,
+				None => {
+					return Err(Error::new(
+						ErrorKind::Other,
+						"A Public Key file must be specified.",
+					));
+				}
+			};
+			match gen.value_of("mode") {
+				Some(m) => {
+					let m = m.trim();
+					if m.len() > 1 {
+						return Err(Error::new(ErrorKind::Other, "Invalid Mode specified."));
+					}
+					match m.chars().next().unwrap() {
+						'q' => gen_quantum_keypair(public_key, secret_key)?,
+						'c' => gen_classical_keypair(public_key, secret_key)?,
+						'h' => gen_hybrid_keypair(public_key, secret_key)?,
+						_ => {
+							return Err(Error::new(ErrorKind::Other, "Invalid Mode specified."));
+						}
+					}
+				}
+				None => return Err(Error::new(ErrorKind::Other, "A mode must be specified.")),
+			};
+		}
+		unimplemented!();
 	}
 	Ok(())
 }
