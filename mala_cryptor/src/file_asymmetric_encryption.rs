4f4fe068e9f3d64a78159e5843227a92947d60b1
ValliereMagic <ValliereMagic@users.noreply.github.com>
2021-10-31 16:24:09 -0400

    Initial implementation of quantum pub-sec encryption.

--- mala_cryptor/src/file_asymmetric_encryption.rs
+++ mala_cryptor/src/file_asymmetric_encryption.rs
@@ -2,16 +2,14 @@ use crate::enc_algos_in_use::*;
 use crate::file_symmetric_encryption::*;
 use crate::global_constants::*;
 use crate::key_file::*;
-use oqs::kem;
-use oqs::sig;
 use sodiumoxide::crypto::generichash::{State, DIGEST_MAX};
 use sodiumoxide::crypto::secretstream::{Key, KEYBYTES};
 use std::convert::TryInto;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
+use std::io::Result;
 use std::io::SeekFrom;
-use std::io::{Error, ErrorKind, Result};
 
 pub fn encrypt_quantum(
 	dest_pkey_path: &str,
@@ -24,11 +22,19 @@ pub fn encrypt_quantum(
 	// Retrieve the required keys from files
 	let dest_pkey = q.get_pub(dest_pkey_path)?;
 	let skey = q.get_sec(skey_path)?;
-	let pkey = q.get_pub(pkey_path)?;
+	let _ = q.get_pub(pkey_path)?;
 	let kem = get_q_kem_algo();
 	// Derive the shared secret
 	let (ct, ss) = kem.encapsulate(&dest_pkey.1).unwrap();
-	let (mut file_in, mut file_out) = (File::open(file_in_path)?, File::create(file_out_path)?);
+	let (mut file_in, mut file_out) = (
+		File::open(file_in_path)?,
+		OpenOptions::new()
+			.create(true)
+			.read(true)
+			.write(true)
+			.truncate(true)
+			.open(file_out_path)?,
+	);
 	// Write out the ciphertext of the shared secret into the file
 	file_out.write_all(ct.as_ref())?;
 	// Encrypt the source file with the shared secret, and write it to the out
@@ -66,7 +72,7 @@ pub fn decrypt_quantum(
 	// Retrieve the required keys from files
 	let sender_pkey = q.get_pub(sender_pub_key_path)?;
 	let skey = q.get_sec(skey_path)?;
-	let pkey = q.get_pub(pkey_path)?;
+	let _ = q.get_pub(pkey_path)?;
 	let (mut file_in, mut file_out) = (
 		OpenOptions::new()
 			.read(true)
@@ -74,12 +80,11 @@ pub fn decrypt_quantum(
 			.open(file_in_path)?,
 		File::create(file_out_path)?,
 	);
-	// Seek to the signature and pull it in
 	let sig = get_q_sig_algo();
 	// Seek to the beginning of the signature
-	let signature_offset = -(sig.length_signature() as i64);
-	file_in.seek(SeekFrom::End(signature_offset))?;
-	let mut buff = vec![0u8; sig.length_signature()];
+	let signature_offset = sig.length_signature() as i64;
+	file_in.seek(SeekFrom::End(-signature_offset))?;
+	let mut buff = vec![0u8; signature_offset as usize];
 	file_in.read_exact(&mut buff)?;
 	let signature = sig
 		.signature_from_bytes(&buff)
@@ -94,11 +99,11 @@ pub fn decrypt_quantum(
 		Err(_) => panic!("Signature is bad. Not attempting to decrypt file. Aborting."),
 	}
 	// Truncate the signature from the end of the file
-	file_in.set_len((file_in.metadata().unwrap().len() as i64 + signature_offset) as u64)?;
-	// Seek back to the beginning of the file
-	file_in.rewind()?;
+	file_in.set_len((file_in.metadata().unwrap().len() as i64 - signature_offset) as u64)?;
 	let kem = get_q_kem_algo();
 	// Read in the key exchange ciphertext
+	// Rewind the file to the beginning first
+	file_in.rewind()?;
 	let mut kem_ct_buff = vec![0u8; kem.length_ciphertext()];
 	file_in.read_exact(&mut kem_ct_buff)?;
 	let ct = kem
@@ -113,7 +118,7 @@ pub fn decrypt_quantum(
 	decrypt_file(
 		&mut file_in,
 		&mut file_out,
-		Key(ss.as_ref()[0..32]
+		Key(ss.as_ref()[0..KEYBYTES]
 			.try_into()
 			.expect("Unable to turn shared secret into symmetric key.")),
 	)?;
@@ -122,33 +127,32 @@ pub fn decrypt_quantum(
 
 fn digest(file: &mut File, signature_avoid: Option<i64>) -> Result<[u8; DIGEST_MAX]> {
 	// Figure out how many chunks we will iterate through
-	let mut file_len: u64 = file.metadata().unwrap().len();
+	let mut file_len: i64 = file.metadata().unwrap().len() as i64;
 	// If signature_avoid is present, decrease the file_len to remove the
 	// signature from being read into the digest.
 	if let Some(offset) = signature_avoid {
-		let file_len_i = (file_len as i64) + offset;
-		if file_len_i <= 0 {
+		file_len -= offset;
+		if file_len <= 0 {
 			panic!("Digesting negative or zero-sized file.");
 		}
-		file_len = file_len_i as u64;
 	}
 	let mut state = State::new(Some(DIGEST_MAX), None).expect("Unable to create message state");
 	let mut buff = [0u8; CHUNK_SIZE];
 	// digest the file in chunks
 	while file_len > 0 {
 		// Only read what's left in the file
-		let chunk_size = if file_len >= CHUNK_SIZE as u64 {
+		let chunk_size = if file_len > CHUNK_SIZE as i64 {
 			CHUNK_SIZE
 		} else {
-			CHUNK_SIZE - file_len as usize
+			file_len as usize
 		};
-		let read_bytes = file.read(&mut buff[0..chunk_size])?;
+		file.read_exact(&mut buff[0..chunk_size])?;
 		// Add the bytes read to the digest
 		state
-			.update(&buff[0..read_bytes])
+			.update(&buff[0..chunk_size])
 			.expect("Unable to update message state");
 		// Remove the read bytes from file_len
-		file_len -= read_bytes as u64;
+		file_len -= chunk_size as i64;
 	}
 	Ok(
 		state.finalize().expect("Unable to finalize message digest")[0..DIGEST_MAX]
